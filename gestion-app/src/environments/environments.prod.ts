c87967dadd1b7b6689741490545f1b1bb8e199da
stevrdz <stevenrodriguezm97@gmail.com>
2025-12-15 05:29:49 -0500

    fix: Variables de entorno - Angular

---
+++ gestion-app/src/environments/environments.prod.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+  
+    authApiBaseUrl: '/auth-api/api',
+  
+    accessTokenStorageKey: 'Kx9pL3rQ8nV1zT6wM2bF4hR7cY5gP0kA3sD9jL8uN2rX7v',
+  };
+  
---
+++ gestion-app/src/environments/environments.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+  
+    authApiBaseUrl: '/auth-api/api',
+  
+    accessTokenStorageKey: 'Kx9pL3rQ8nV1zT6wM2bF4hR7cY5gP0kA3sD9jL8uN2rX7v',
+  };
+  
